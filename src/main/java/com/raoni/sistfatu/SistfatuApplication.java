76a31f8fbf46368106a2aee7993d087e2bfbf524
Raoni Moreira <raonicmoreira@gmail.com>
2018-08-22 19:47:48 -0300

    Criando Projeto SistFatu

---
+++ src/main/java/com/raoni/sistfatu/SistfatuApplication.java
@@ -0,0 +1,12 @@
+package com.raoni.sistfatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistfatuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistfatuApplication.class, args);
+	}
+}
---
+++ src/test/java/com/raoni/sistfatu/SistfatuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.raoni.sistfatu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistfatuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
